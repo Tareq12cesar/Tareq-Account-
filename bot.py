d27883b8fbc691c70ba007f0862ce094c7614995
Tareq12cesar <tareqjulio@gmail.com>
2025-06-03 00:03:03 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -258,11 +258,12 @@ def get_skin_count(message, skin_type):
     )
     send_skin_selection_menu(message.chat.id)
 # ======= سیستم اکانت درخواستی =======
-def get_collection(message):
-    if check_back(message): return
-    user_data[message.chat.id]['collection'] = message.text
-    bot.send_message(message.chat.id, "لطفاً اسکین‌های مهم اکانت را وارد کنید:")
-    bot.register_next_step_handler(message, get_key_skins)
+    @bot.message_handler(func=lambda message: message.text == " اکانت درخواستی ")
+def start_buy_request(message):
+    user_data[message.chat.id] = {'username': message.from_user.username}
+    bot.send_message(message.chat.id, "🔍 اسکین‌هایی که می‌خوای تو اکانت باشه رو تایپ کن:")
+    bot.register_next_step_handler(message, get_requested_skins)
+
 
 def get_requested_skins(message):
     if check_back(message): return
@@ -367,7 +368,7 @@ def handle_admin_text(message):
         user_id = pending['user_id']
         if pending.get('type') == 'buy':
             bot.send_message(user_id, f"❌ متأسفانه درخواست شما رد شد.\nدلیل: {reason}")
-
+    
     
 # ======= اجرای ربات با Flask =======
 app = Flask(__name__)
