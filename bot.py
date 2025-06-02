a06dc4a01040bf39b2173e25c54ebbcd3e342dd3
Tareq12cesar <tareqjulio@gmail.com>
2025-06-02 04:07:38 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -173,16 +173,16 @@ user_data = {}
 def send_skin_selection_menu(chat_id):
 # ایجاد کیبورد جدید ستونی
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True, row_width=1)
-        markup.add(
+    markup.add(
         "Supreme",
         "Grand",
         "Exquisite",
         "Deluxe",
         "قیمت نهایی",
         "بازگشت"
-   )
+    )
+    bot.send_message(chat_id, "✅ لطفاً نوع اسکین‌های خود را انتخاب کنید:", reply_markup=markup)
 
-bot.send_message(chat_id, "✅ لطفاً نوع اسکین‌های خود را انتخاب کنید:", reply_markup=markup)
 @bot.message_handler(func=lambda message: message.text in ["Supreme", "Grand", "Exquisite", "Deluxe", "قیمت نهایی", "بازگشت"])
 def calculate_price(message):
     if check_back(message):
