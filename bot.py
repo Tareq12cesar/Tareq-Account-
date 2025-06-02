f5a3c64df98c4807e6674d21c97d49fb86a16343
Tareq12cesar <tareqjulio@gmail.com>
2025-06-02 04:51:15 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -213,8 +213,7 @@ def calculate_price(message):
 
         final_message = "💵 قیمت نهایی کل اسکین‌ها:\n\n" + "\n".join(summary_lines) + f"\n\n💰 جمع کل: {total_price:,} تومان\n\n💡 قیمت بالا ارزش اکانت شماست\nبرای ثبت آگهی تو کانال، قیمت فروش رو خودتون تعیین می‌کنید."
         bot.send_message(message.chat.id, final_message)
-        send_skin_selection_menu(message.chat.id)
-        return
+        send_main_menu(message.chat.id)
 
     valid_skin_types = ["Supreme", "Grand", "Exquisite", "Deluxe"]
     if text in valid_skin_types:
