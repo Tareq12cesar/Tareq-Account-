ca4081a0e546fc0743ef35202af0c09c6d2c0393
Tareq12cesar <tareqjulio@gmail.com>
2025-06-06 19:43:52 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -271,13 +271,13 @@ def get_skin_count(message, skin_type):
         return
 
     try:
-    count = int(message.text.strip())
-    if count < 0:
-        raise ValueError()
-except Exception:
-    bot.send_message(message.chat.id, "❌ لطفاً فقط عدد مثبت وارد کنید. چندتا اسکین داری؟")
+        count = int(message.text.strip())
+        if count < 0:
+            raise ValueError()
+    except Exception:
+        bot.send_message(message.chat.id, "❌ لطفاً فقط عدد مثبت وارد کنید. چندتا اسکین داری؟")
     send_skin_selection_menu(message.chat.id)
-    return
+        return
 
     user_data[message.chat.id][skin_type] = count
 
