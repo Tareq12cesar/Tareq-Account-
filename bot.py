cba66640d465ea32f3c31c2273a9c070d56792e9
Tareq12cesar <tareqjulio@gmail.com>
2025-08-25 15:25:12 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from flask import Flask, request
 import threading
 
 # ======= تنظیمات اولیه =======
-BOT_TOKEN = '7963209844:AAF3CHMa41bUwBXFVkq4HcNTCjMGUTzS2UI'
+BOT_TOKEN = '7963209844:AAE_LrPsVyxE3q_GgcdF8FOebrix6xjFUps'
 ADMIN_ID = 6697070308  # آیدی عددی ادمین
 CHANNEL_USERNAME = '@TareqMlbb'  # یوزرنیم کانال
 CHANNEL_LINK = 'https://t.me/TareqMlbb'
